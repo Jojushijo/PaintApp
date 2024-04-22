949fad9acc8ee5a8435bd8a6f61df24db60f9ccd
hakase <davidsommerfield.zg@gmail.com>
2024-04-22 01:50:39 -0500

    replacement color looping bug fix

--- src/bucket.cpp
+++ src/bucket.cpp
@@ -29,26 +29,29 @@ void Bucket::set_start(int x, int y, std::array<Uint8, 3>& color) {
 
 void Bucket::flood_fill(int x, int y, std::array<Uint8, 3>& replace_color, std::array<Uint8, 3>& new_color) {
     std::stack<std::pair<int, int>> pixels;
-    pixels.push(std::make_pair(x, y));
 
-    while (!pixels.empty()) {
-        auto [u, v] = pixels.top();
-        pixels.pop();
+    if (replace_color != new_color) {
+        pixels.push(std::make_pair(x, y));
 
-        if (u >= 0 && u < canvas.surface->w && v >= 0 && v < canvas.surface->h) {
-            int index = v * canvas.surface->pitch + u * canvas.surface->format->BytesPerPixel;
-            Uint8* pixel = reinterpret_cast<Uint8*>(canvas.surface->pixels) + index;
+        while (!pixels.empty()) {
+            auto [u, v] = pixels.top();
+            pixels.pop();
 
-            if ((pixel[0] == replace_color[0]) && (pixel[1] == replace_color[1]) && (pixel[2] == replace_color[2])) {
-                pixel[0] = new_color[0];
-                pixel[1] = new_color[1];
-                pixel[2] = new_color[2];
+            if (u >= 0 && u < canvas.surface->w && v >= 0 && v < canvas.surface->h) {
+                int index = v * canvas.surface->pitch + u * canvas.surface->format->BytesPerPixel;
+                Uint8* pixel = reinterpret_cast<Uint8*>(canvas.surface->pixels) + index;
 
-                // Add neighbors to the stack
-                pixels.push(std::make_pair(u - 1, v)); // left
-                pixels.push(std::make_pair(u + 1, v)); // right
-                pixels.push(std::make_pair(u, v - 1)); // up
-                pixels.push(std::make_pair(u, v + 1)); // down
+                if ((pixel[0] == replace_color[0]) && (pixel[1] == replace_color[1]) && (pixel[2] == replace_color[2])) {
+                    pixel[0] = new_color[0];
+                    pixel[1] = new_color[1];
+                    pixel[2] = new_color[2];
+
+                    // Add neighbors to the stack
+                    pixels.push(std::make_pair(u - 1, v)); // left
+                    pixels.push(std::make_pair(u + 1, v)); // right
+                    pixels.push(std::make_pair(u, v - 1)); // up
+                    pixels.push(std::make_pair(u, v + 1)); // down
+                }
             }
         }
     }
